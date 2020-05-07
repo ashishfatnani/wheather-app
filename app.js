b8fe0fe876d18c150a1e6184f067bac43f3ad813
ashishfatnani <ashishfatnani3@gmail.com>
2020-05-07 22:47:52 +0530

    heroku commit

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/properties", require("./routes/properties"));
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 app.listen(port, () => {
   console.log(`Serving to the port number ${port}`);
 });
